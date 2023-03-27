6f5ad6df4c348a53310640d7d7307d5c589fb588
Sigfrid Stjärnholm <sigge.stjarnholm@gmail.com>
2023-03-27 22:32:34 +0200

    Update logger

--- starbun/logger.py
+++ starbun/logger.py
@@ -1,31 +1,44 @@
 from termcolor import colored
 
+def format_labels(experiment_label=None, trial_label=None, run_label=None):
+    if not experiment_label and not trial_label and not run_label:
+        return None
+
+    labels = [experiment_label, trial_label, run_label]
+    label = f"{' / '.join([label for label in labels if label])} |"
+    return colored(label, "grey")
+
 class Logger:
     def __init__(self, name):
         if not name:
             raise ValueError("Logger name cannot be empty")
         self.name = name
 
-    def log(self, message, color=None):
+    def log(self, message, level_string, level_color, color=None,
+            experiment_label=None, trial_label=None, run_label=None):
         name_string = colored(f"[{self.name}]", "cyan")
+        level_string = colored(f"{str.ljust(level_string, 6)}", level_color, attrs=["bold"])
+        labels = format_labels(experiment_label=experiment_label, trial_label=trial_label, run_label=run_label)
+        prefix = " ".join(filter(lambda item: item is not None, [name_string, level_string, labels]))
+
         if color:
-            print(name_string, colored(message, color))
+            print(prefix, colored(message, color))
         else:
-            print(name_string, message)
+            print(prefix, message)
 
-    def info(self, message):
-        self.log(message, "blue")
+    def info(self, message, experiment_label=None, trial_label=None, run_label=None):
+        self.log(message, level_string="info", level_color="blue", experiment_label=experiment_label, trial_label=trial_label, run_label=run_label)
 
-    def warn(self, message):
-        self.log(message, "yellow")
+    def warn(self, message, experiment_label=None, trial_label=None, run_label=None):
+        self.log(message, level_string="warn", level_color="yellow", experiment_label=experiment_label, trial_label=trial_label, run_label=run_label)
 
-    def error(self, message):
-        self.log(message, "red")
+    def error(self, message, experiment_label=None, trial_label=None, run_label=None):
+        self.log(message, level_string="error", level_color="red", experiment_label=experiment_label, trial_label=trial_label, run_label=run_label)
 
-    def debug(self, message):
-        self.log(message, "grey")
+    def debug(self, message, experiment_label=None, trial_label=None, run_label=None):
+        self.log(message, level_string="debug", level_color="grey", experiment_label=experiment_label, trial_label=trial_label, run_label=run_label)
 
-    def fatal(self, message):
-        self.log(message, "magenta")
+    def fatal(self, message, experiment_label=None, trial_label=None, run_label=None):
+        self.log(message, level_string="fatal", level_color="magenta", experiment_label=experiment_label, trial_label=trial_label, run_label=run_label)
 
 logger = Logger("Starbun")
--- tests/test_starbun.py
+++ tests/test_starbun.py
@@ -4,10 +4,19 @@ from starbun.logger import logger
 class TestStarbun(unittest.TestCase):
 
 	def test_starbun(self):
-		logger.info("info")
-		logger.warn("warn")
-		logger.error("error")
-		logger.debug("debug")
-		logger.fatal("fatal")
+		experiment = "PROJECT"
+		trial = "T1"
+		run = "R1"
+		logger.info("this is a test")
+		logger.warn("this is a test")
+		logger.error("this is a test")
+		logger.debug("this is a test")
+		logger.fatal("this is a test")
+
+		logger.info("this is a test", experiment_label=experiment)
+		logger.warn("this is a test", experiment_label=experiment, trial_label=trial)
+		logger.error("this is a test", experiment_label=experiment, trial_label=trial, run_label=run)
+		logger.debug("this is a test", trial_label=trial, run_label=run)
+		logger.fatal("this is a test")
 
 unittest.main()
